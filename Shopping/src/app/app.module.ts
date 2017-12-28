14203368d13271b4e5a6d56d03dea2044f2a98a5
Administrator@HOMEDEV.COM <Administrator@HOMEDEV.COM>
2017-12-27 23:16:13 -0500

    commit

--- Shopping/src/app/app.module.ts
+++ Shopping/src/app/app.module.ts
@@ -11,6 +11,8 @@ import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.com
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { FropdownDirective } from './shared/directives/fropdown.directive';
+import { DropdownDirective } from './shared/directives/dropdown.directive';
 
 @NgModule({
   declarations: [
@@ -21,7 +23,9 @@ import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-ed
     RecipeDetailComponent,
     RecipeItemComponent,
     ShoppingListComponent,
-    ShoppingEditComponent
+    ShoppingEditComponent,
+    FropdownDirective,
+    DropdownDirective
   ],
 
   imports: [
