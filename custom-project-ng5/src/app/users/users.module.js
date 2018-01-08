130fecadf3f47e5a048976aba681d8bbf3b6cb74
administrator@HOMEDEV.COM <administrator@HOMEDEV.COM>
2018-01-07 21:53:07 -0500

    commit

---
+++ custom-project-ng5/src/app/users/users.module.js
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UsersComponent } from './users.component';
+import { UsersRoutingModule } from './users-routing.module';
+var UsersModule = /** @class */ (function () {
+    function UsersModule() {
+    }
+    return UsersModule;
+}());
+export { UsersModule };
